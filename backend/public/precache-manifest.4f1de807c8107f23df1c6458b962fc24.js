25ca333b3a99c3279d6cedf10c8234a1ffb5b33b
Louis Lepper <me@louislepper.co.nz>
2020-08-20 18:58:46 +1000

    Pulled in latest frontend.
     Note that this relies on breaking changes
    coming in the next commit.

---
+++ backend/public/precache-manifest.4f1de807c8107f23df1c6458b962fc24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4107c35c55d9c8ad98ebd22870fce88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bded7acb92818b8c89f0",
+    "url": "/static/css/2.12de1369.chunk.css"
+  },
+  {
+    "revision": "4c21add5122844f235d2",
+    "url": "/static/css/main.342e5bcd.chunk.css"
+  },
+  {
+    "revision": "bded7acb92818b8c89f0",
+    "url": "/static/js/2.c9f105ef.chunk.js"
+  },
+  {
+    "revision": "fc024ed682d2fa67df54e53f7fb6b674",
+    "url": "/static/js/2.c9f105ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c21add5122844f235d2",
+    "url": "/static/js/main.575b978e.chunk.js"
+  },
+  {
+    "revision": "ffd88fbc1877c46f8775",
+    "url": "/static/js/runtime-main.76876a73.js"
+  }
+]);
--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c70db50ccfdac2470b216ac7d7858e2.js"
+  "/precache-manifest.4f1de807c8107f23df1c6458b962fc24.js"
 );
 
 self.addEventListener('message', (event) => {
